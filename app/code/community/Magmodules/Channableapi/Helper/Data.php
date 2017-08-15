dae29bf5cbc12230e9daff170a43fb5a92c5e8af
Magmodules <info@magmodules.eu>
2017-08-15 14:34:37 +0200

    1.1.6

--- app/code/community/Magmodules/Channableapi/Helper/Data.php
+++ app/code/community/Magmodules/Channableapi/Helper/Data.php
@@ -31,12 +31,12 @@ class Magmodules_Channableapi_Helper_Data extends Mage_Core_Helper_Abstract
     {
         $test = $request->getParam('test');
         if ($test) {
-           return $this->getTestJsonData($test);
+            return $this->getTestJsonData($test);
         } else {
-			$data = json_decode($data, true);
-			if (json_last_error() != JSON_ERROR_NONE) {
-				return $this->jsonResponse('Post not valid JSON-Data: ' . json_last_error_msg());
-			}
+            $data = json_decode($data, true);
+            if (json_last_error() != JSON_ERROR_NONE) {
+                return $this->jsonResponse('Post not valid JSON-Data: ' . json_last_error_msg());
+            }
             if (empty($data)) {
                 return $this->jsonResponse('No Order Data in post');
             }
@@ -48,6 +48,52 @@ class Magmodules_Channableapi_Helper_Data extends Mage_Core_Helper_Abstract
             }
             return $data;
         }
+    }
+
+    /**
+     * @param $productId
+     *
+     * @return string
+     */
+    public function getTestJsonData($productId)
+    {
+        $product = Mage::getModel('catalog/product')->load($productId);
+        if ($product) {
+            $data = '{"channable_id": 112345, "channel_id": 12345678, "channel_name": "Bol", "extra": {"memo": "Channable Test", "comment": "Channable order id: 999999999"}, "price": {"total": ' . $product->getFinalPrice() . ', "currency": "EUR", "shipping": 0, "subtotal": ' . $product->getFinalPrice() . ', "commission": 2.50, "payment_method": "bol", "transaction_fee": 0}, "billing": { "city": "Amsterdam", "state": "", "email": "dontemail@me.net", "address_line_1": "Billing Line 1", "address_line_2": "Billing Line 2", "street": "Donkere Spaarne", "company": "Test company", "zip_code": "5000 ZZ", "last_name": "Channable", "first_name": "Test", "middle_name": "from", "country_code": "NL", "house_number": 100, "house_number_ext": "a", "address_supplement": "Address supplement" }, "customer": { "email": "dontemail@me.net", "phone": "054333333", "gender": "man", "mobile": "", "company": "Test company", "last_name": "From Channable", "first_name": "Test", "middle_name": "" }, "products": [{"id": "' . $product->getEntityId() . '", "ean": "000000000", "price": ' . $product->getFinalPrice() . ', "title": "' . htmlentities($product->getName()) . '", "quantity": 1, "shipping": 0, "commission": 2.50, "reference_code": "00000000", "delivery_period": "2016-07-12+02:00"}], "shipping": {  "city": "Amsterdam", "state": "", "email": "dontemail@me.net", "street": "Shipping Street", "company": "Magmodules", "zip_code": "1000 AA", "last_name": "from Channable", "first_name": "Test order", "middle_name": "", "country_code": "NL", "house_number": 21, "house_number_ext": "B", "address_supplement": "Address Supplement", "address_line_1": "Shipping Line 1", "address_line_2": "Shipping Line 2" }}';
+            return json_decode($data, true);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $errors
+     * @param string $orderId
+     * @param string $channableId
+     *
+     * @return mixed
+     */
+    public function jsonResponse($errors = '', $orderId = '', $channableId = '')
+    {
+        $response = array();
+        if (!empty($orderId)) {
+            $response['validated'] = 'true';
+            $response['order_id'] = $orderId;
+        } else {
+            $response['validated'] = 'false';
+            $response['errors'] = $errors;
+        }
+
+        $logEnabled = Mage::getStoreConfig('channable_api/debug/log');
+        if ($logEnabled) {
+            if ($response['validated'] == 'true') {
+                Mage::getModel('channableapi/debug')->orderSuccess($orderId);
+            } else {
+                Mage::getModel('channableapi/debug')->orderError($errors, $channableId);
+            }
+        }
+
+        return $response;
     }
 
     /**
@@ -88,52 +134,6 @@ class Magmodules_Channableapi_Helper_Data extends Mage_Core_Helper_Abstract
 
         if ($code != $token) {
             return $this->jsonResponse('Invalid token');
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $errors
-     * @param string $orderId
-     * @param string $channableId
-     *
-     * @return mixed
-     */
-    public function jsonResponse($errors = '', $orderId = '', $channableId = '')
-    {
-        $response = array();
-        if (!empty($orderId)) {
-            $response['validated'] = 'true';
-            $response['order_id'] = $orderId;
-        } else {
-            $response['validated'] = 'false';
-            $response['errors'] = $errors;
-        }
-
-        $logEnabled = Mage::getStoreConfig('channable_api/debug/log');
-        if ($logEnabled) {
-            if ($response['validated'] == 'true') {
-                Mage::getModel('channableapi/debug')->orderSuccess($orderId);
-            } else {
-                Mage::getModel('channableapi/debug')->orderError($errors, $channableId);
-            }
-        }
-
-        return $response;
-    }
-
-    /**
-     * @param $productId
-     *
-     * @return string
-     */
-    public function getTestJsonData($productId)
-    {
-        $product = Mage::getModel('catalog/product')->load($productId);
-        if ($product) {
-            $data = '{"channable_id": 112345, "channel_id": 12345678, "channel_name": "Bol", "extra": {"memo": "Channable Test", "comment": "Channable order id: 999999999"}, "price": {"total": ' . $product->getFinalPrice() . ', "currency": "EUR", "shipping": 0, "subtotal": ' . $product->getFinalPrice() . ', "commission": 2.50, "payment_method": "bol", "transaction_fee": 0}, "billing": { "city": "Amsterdam", "state": "", "email": "dontemail@me.net", "address_line_1": "Billing Line 1", "address_line_2": "Billing Line 2", "street": "Donkere Spaarne", "company": "Test company", "zip_code": "5000 ZZ", "last_name": "Channable", "first_name": "Test", "middle_name": "from", "country_code": "NL", "house_number": 100, "house_number_ext": "a", "address_supplement": "Address supplement" }, "customer": { "email": "dontemail@me.net", "phone": "054333333", "gender": "man", "mobile": "", "company": "Test company", "last_name": "From Channable", "first_name": "Test", "middle_name": "" }, "products": [{"id": "' . $product->getEntityId() . '", "ean": "000000000", "price": ' . $product->getFinalPrice() . ', "title": "' . htmlentities($product->getName()) . '", "quantity": 1, "shipping": 0, "commission": 2.50, "reference_code": "00000000", "delivery_period": "2016-07-12+02:00"}], "shipping": {  "city": "Amsterdam", "state": "", "email": "dontemail@me.net", "street": "Shipping Street", "company": "Magmodules", "zip_code": "1000 AA", "last_name": "from Channable", "first_name": "Test order", "middle_name": "", "country_code": "NL", "house_number": 21, "house_number_ext": "B", "address_supplement": "Address Supplement", "address_line_1": "Shipping Line 1", "address_line_2": "Shipping Line 2" }}';
-			return json_decode($data, true);
         }
 
         return false;
